b1d7e706d544649dbd663605e0131a55cf630b42
bgayatri3 <gayatri.baskaran1@gmail.com>
2024-12-04 18:39:22 -0500

    applied font globally

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,30 +13,40 @@ import Loading from './pages/Loading';
 import SignUp from './pages/SignUp';
 import { useLoading } from './contexts/LoadingContext';
 import { UserProvider } from './contexts/UserContext';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+
+// Create a custom MUI theme with Special Elite font
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Special Elite", sans-serif', // Apply Special Elite font globally
+  },
+});
 
 const App = () => {
   const { loading } = useLoading();
 
   return (
     <UserProvider>
-      <Router>
-        <div className="App">
-          <Header />
-          {loading && <Loading />}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/affirmation" element={<Affirmation />} />
-            <Route path="/about" element={<About />} />
-            <Route path='/mindfulminutes' element={<MindfulMinutes/>} />
-            {/**<Route path="/profile/anonymous" element={<AnonymousProfile />} />**/}
-            {/* <Route path="/profile/anonymous" element={<AnonymousProfile />} /> */}
-            <Route path="/profile/:username" element={<Profile />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="*" element={<ErrorPage message="Page not found" />} />
-          </Routes>
-        </div>
-      </Router>
+      <ThemeProvider theme={theme}> {/* Wrap the whole app with the ThemeProvider */}
+        <CssBaseline /> {/* This ensures that the global styles from the theme are applied */}
+        <Router>
+          <div className="App">
+            <Header />
+            {loading && <Loading />}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/affirmation" element={<Affirmation />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/mindfulminutes" element={<MindfulMinutes />} />
+              <Route path="/profile/:username" element={<Profile />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="*" element={<ErrorPage message="Page not found" />} />
+            </Routes>
+          </div>
+        </Router>
+      </ThemeProvider>
     </UserProvider>
   );
 };
