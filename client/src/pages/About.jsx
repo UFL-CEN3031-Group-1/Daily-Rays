3ab6d980df2d0c3afba6c83448ca0dea831b5f20
Ricky Zhang <rickyzhang196@outlook.com>
2024-12-03 20:55:10 -0500

    styling

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <div>
             <h1>About Page</h1>
-            <p>This is the About Page!</p>
+            <p>Daily Rays is an app about spreading positive affirmations to lighten other peoples' days. Yippee!</p>
         </div>
     );
 };
